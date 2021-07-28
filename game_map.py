9d1dd7ca0ebe2d815a130e7ba439c932c76b4726
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-07-27 21:03:35 -0400

    Update game_map.py

--- game_map.py
+++ game_map.py
@@ -5,7 +5,7 @@ from typing import Iterable, Iterator, Optional, TYPE_CHECKING
 import numpy as np  # type: ignore
 from tcod.console import Console
 
-from entity import Actor
+from entity import Actor, Item
 import tile_types
 
 if TYPE_CHECKING:
@@ -29,6 +29,10 @@ class GameMap:
         )  # Tiles the player has seen before
     
     @property
+    def gamemap(self) -> GameMap:
+        return self
+    
+    @property
     def actors(self) -> Iterator[Actor]:
         """Iterate over this maps living actors."""
         yield from (
@@ -36,6 +40,10 @@ class GameMap:
             for entity in self.entities
             if isinstance(entity, Actor) and entity.is_alive
         )
+    
+    @property
+    def items(self) -> Iterator[Item]:
+        yield from (entity for entity in self.entities if isinstance(entity, Item))
     
     def get_blocking_entity_at_location(
         self, location_x: int, location_y: int,
