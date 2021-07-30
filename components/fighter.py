eff232623680ea1420e6fb3a712dd3fb661a9837
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-07-29 23:36:08 -0400

    Update fighter.py

--- components/fighter.py
+++ components/fighter.py
@@ -4,7 +4,6 @@ from typing import TYPE_CHECKING
 
 import color
 from components.base_component import BaseComponent
-from input_handlers import GameOverEventHandler
 from render_order import RenderOrder
 
 if TYPE_CHECKING:
@@ -34,7 +33,6 @@ class Fighter(BaseComponent):
         if self.engine.player is self.parent:
             death_message = "You died!"
             death_message_color = color.player_die
-            self.engine.event_handler = GameOverEventHandler(self.engine)
         else:
             death_message = f"{self.parent.name} is dead!"
             death_message_color = color.enemy_die
@@ -47,6 +45,8 @@ class Fighter(BaseComponent):
         self.parent.render_order = RenderOrder.CORPSE
 
         self.engine.message_log.add_message(death_message, death_message_color)
+        
+        self.engine.player.level.add_xp(self.parent.level.xp_given)
     
     def heal(self, amount: int) -> int:
         if self.hp == self.max_hp:
