b059a422fb2054e585bdb0fdb7efd86787f699ce
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-07-29 23:36:18 -0400

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,26 +1,26 @@
 from __future__ import annotations
 
+import lzma
+import pickle
 from typing import TYPE_CHECKING
 
 from tcod.console import Console
 from tcod.map import compute_fov
 
 import exceptions
-from input_handlers import MainGameEventHandler
 from message_log import MessageLog
-from render_functions import render_bar, render_names_at_mouse_location
+import render_functions
 
 if TYPE_CHECKING:
     from entity import Actor
-    from game_map import GameMap
-    from input_handlers import EventHandler
+    from game_map import GameMap, GameWorld
 
 
 class Engine:
     game_map: GameMap
+    game_world: GameWorld
     
     def __init__(self, player: Actor):
-        self.event_handler: EventHandler = MainGameEventHandler(self)
         self.message_log = MessageLog()
         self.mouse_location = (0, 0)
         self.player = player
@@ -48,11 +48,25 @@ class Engine:
         
         self.message_log.render(console=console, x=21, y=45, width=40, height=5)
         
-        render_bar(
+        render_functions.render_bar(
             console=console,
             current_value=self.player.fighter.hp,
             maximum_value=self.player.fighter.max_hp,
             total_width=20,
         )
         
-        render_names_at_mouse_location(console=console, x=21, y=44, engine=self)
+        render_functions.render_dungeon_level(
+            console=console,
+            dungeon_level=self.game_world.current_floor,
+            location=(0, 47),
+        )
+
+        render_functions.render_names_at_mouse_location(
+            console=console, x=21, y=44, engine=self
+        )
+    
+    def save_as(self, filename: str) -> None:
+        """Save this Engine instance as a compressed file."""
+        save_data = lzma.compress(pickle.dumps(self))
+        with open(filename, "wb") as f:
+            f.write(save_data)
