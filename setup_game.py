7611d1b1bef07207b901b4e00ed21b105a92d715
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-08-17 15:34:46 -0400

    Update setup_game.py

--- setup_game.py
+++ setup_game.py
@@ -83,7 +83,7 @@ class MainMenu(input_handlers.BaseEventHandler):
         console.print(
             console.width // 2,
             console.height // 2 - 4,
-            "TOMBS OF THE ANCIENT KINGS",
+            "Heists Ltd.",
             fg=color.menu_title,
             alignment=tcod.CENTER,
         )
