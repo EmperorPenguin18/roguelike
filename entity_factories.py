2602abe8ea96b29aba6e936bd80c0fdb94c137ce
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-08-17 18:07:25 -0400

    Update entity_factories.py

--- entity_factories.py
+++ entity_factories.py
@@ -18,9 +18,9 @@ player = Actor(
 )
 
 orc = Actor(
-    char="o",
+    char="g",
     color=(63, 127, 63),
-    name="Orc",
+    name="Guard",
     ai_cls=HostileEnemy,
     equipment=Equipment(),
     fighter=Fighter(hp=10, base_defense=0, base_power=3),
@@ -28,9 +28,9 @@ orc = Actor(
     level=Level(xp_given=35),
 )
 troll = Actor(
-    char="T",
+    char="S",
     color=(0, 127, 0),
-    name="Troll",
+    name="SWAT",
     ai_cls=HostileEnemy,
     equipment=Equipment(),
     fighter=Fighter(hp=16, base_defense=1, base_power=4),
@@ -41,41 +41,41 @@ troll = Actor(
 confusion_scroll = Item(
     char="~",
     color=(207, 63, 255),
-    name="Confusion Scroll",
+    name="Delirium Dart",
     consumable=consumable.ConfusionConsumable(number_of_turns=10),
 )
 fireball_scroll = Item(
     char="~",
     color=(255, 0, 0),
-    name="Fireball Scroll",
+    name="Grenade",
     consumable=consumable.FireballDamageConsumable(damage=12, radius=3),
 )
 health_potion = Item(
     char="!",
     color=(127, 0, 255),
-    name="Health Potion",
+    name="Adrenaline Shot",
     consumable=consumable.HealingConsumable(amount=4),
 )
 lightning_scroll = Item(
     char="~",
     color=(255, 255, 0),
-    name="Lightning Scroll",
+    name="Taser",
     consumable=consumable.LightningDamageConsumable(damage=20, maximum_range=5),
 )
 
 dagger = Item(
-    char="/", color=(0, 191, 255), name="Dagger", equippable=equippable.Dagger()
+    char="/", color=(0, 191, 255), name="Pocket Knife", equippable=equippable.Dagger()
 )
 
-sword = Item(char="/", color=(0, 191, 255), name="Sword", equippable=equippable.Sword())
+sword = Item(char="/", color=(0, 191, 255), name="Butterfly Knife", equippable=equippable.Sword())
 
 leather_armor = Item(
     char="[",
     color=(139, 69, 19),
-    name="Leather Armor",
+    name="Suit",
     equippable=equippable.LeatherArmor(),
 )
 
 chain_mail = Item(
-    char="[", color=(139, 69, 19), name="Chain Mail", equippable=equippable.ChainMail()
+    char="[", color=(139, 69, 19), name="Ballistic Vest", equippable=equippable.ChainMail()
 )
