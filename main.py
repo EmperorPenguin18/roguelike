1d343413873325a73e2e0fa21cd9a87803078a50
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-08-17 15:32:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main() -> None:
         screen_width,
         screen_height,
         tileset=tileset,
-        title="Yet Another Roguelike Tutorial",
+        title="Heists Ltd.",
         vsync=True,
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
